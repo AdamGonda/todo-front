bd641b1732881d2a8ebac18862955da132c6236b
AdamGonda <adamgondagyula@gmail.com>
2019-07-16 10:14:35 +0200

    markup is done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [todos, setTodos] = React.useState([
+		{ id: 1, title: 'one', isDone: false },
+		{ id: 2, title: 'two', isDone: true },
+		{ id: 3, title: 'three', isDone: false },
+		{ id: 4, title: 'four', isDone: true }
+	])
+
+	return (
+		<section>
+			<div id="app">
+				<h1 id="title">todos</h1>
+				<div>
+					<div id="header">
+						<input type="checkbox" />
+						<input type="text" />
+					</div>
+					<div id="body">
+						<ul id="todos">
+							{todos.map(todo => (
+								<li className="todo">
+                  <input type="checkbox" />
+                  <input type='text' value={todo.title} />
+                  <button>X</button>
+								</li>
+							))}
+						</ul>
+						<div id="footer">
+              <span id='items-left'>{todos.length} item{todos.length > 1 ? 's' : null} left</span>
+              <div id='controll-panel'>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+              </div>
+              <button>Clear completed</button>
+            </div>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
-export default App;
+export default App
